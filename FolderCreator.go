ca475955ca0dd70441f1e937e23e7484145dfe06
CuculusBand <CuculusBand@outlook.com>
2025-06-08 16:00:49 +0800

    Add main application window

--- FolderCreator.go
+++ FolderCreator.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
+func main() {
+	// createFolderStructure()
+	app := app.New()
+	w0 := app.NewWindow("Folder Creator")
+	w0.Resize(fyne.NewSize(1280, 800))
+	w0.SetFixedSize(true)
+	w0.SetContent(container.NewVBox(widget.NewLabel("<Folder Creator>\n")))
+	w0.Show()
+	print("Folder Creator is running...\n")
+	app.Run()
+	print("Folder Creator is closed.\n")
 }
