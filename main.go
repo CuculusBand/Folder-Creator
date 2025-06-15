bc5c42ec6bd18794d3cff622c2172a9d8d9c84bc
CuculusBand <CuculusBand@outlook.com>
2025-06-15 19:12:09 +0800

    Rewrite main.go

--- main.go
+++ main.go
@@ -1,41 +1,26 @@
 package main
 
 import (
-	"log"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	// createFolderStructure()
-	MyApp := app.New()
+	// Create the application
+	MyApp := app.NewWithID("FolderCreator")
 	// Load and Set the custom font file
 	customFont := fyne.NewStaticResource("NotoSans", LoadFont("assets/font/NotoSans-SemiBold.ttf"))
 	MyApp.Settings().SetTheme(&appTheme{font: customFont})
-	mainWindow := MyApp.NewWindow("Folder Creator")
-	mainWindow.Resize(fyne.NewSize(1280, 800))
-	mainWindow.SetFixedSize(true)
-	//mainWindow.SetContent(container.NewVBox(widget.NewLabel("<Folder Creator>\n")))
-	FileOpenDialog := widget.NewButton("Open File Dialog", func() {
-		log.Println("Open File Dialog button clicked")
-		// Here you would implement the logic to open a file dialog
-		// and handle the selected file path.
-	})
-	content2 := widget.NewButton("destinationPath", func() {
-		log.Println("destinationPathPath button clicked")
-	})
-	// Add both buttons to the main window using a container
-	mainWindow.SetContent(container.NewVBox(
-		widget.NewLabel("<Folder Creator>\n"),
-		FileOpenDialog,
-		content2,
-	))
-	// Run the application
-	mainWindow.Show()
+	// Create the window
+	MainWindow := MyApp.NewWindow("Folder Creator")
+	MainWindow.Resize(fyne.NewSize(600, 800))
+	MainWindow.SetFixedSize(true)
+	MainWindow.CenterOnScreen()
+	app := InitializeApp(MyApp, MainWindow)
+	// Create UI
+	app.MakeUI()
 	print("Folder Creator is running...\n")
-	MyApp.Run()
-	print("Folder Creator is closed.\n")
+	// Run the application
+	MainWindow.ShowAndRun()
+	print("Folder Creator is closed\n")
 }
