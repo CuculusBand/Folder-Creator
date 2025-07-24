79ea9cf6f89e391a9c940f5a00bbefa3e36b2382
CuculusBand <CuculusBand@outlook.com>
2025-07-24 22:45:37 +0800

    Update code comments

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ func main() {
 	// Load and Set the custom font file
 	//customFont := fyne.NewStaticResource("NotoSans", LoadFont("fonts/NotoSans-SemiBold.ttf"))
 	MyApp.Settings().SetTheme(&appTheme{regularFont: AppFont})
-	MyApp.SetIcon(resourceGOFolderCreatorIconPng) // Use bundeled icon
+	MyApp.SetIcon(resourceGOFolderCreatorIconPng) // Use bundled icon
 
 	// Create the window
 	MainWindow := MyApp.NewWindow("Folder Creator")
 	MainWindow.Resize(fyne.NewSize(600, 850))
 	MainWindow.SetFixedSize(false)
 	MainWindow.CenterOnScreen()
-	MainWindow.SetIcon(resourceGOFolderCreatorIconPng) // Use bundeled icon
+	MainWindow.SetIcon(resourceGOFolderCreatorIconPng) // Use bundled icon
 	app := InitializeApp(MyApp, MainWindow)
 	time.Sleep(50 * time.Millisecond)
 	// Create UI
